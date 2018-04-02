7fb75fe1ff2d1765d7c8381c2866a26cfc4a603c
trenflem@gmail.com <trenflem@gmail.com>
2018-04-02 01:52:38 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import logo from './logo.svg';
 import rootReducer from './reducers';
 import './App.css';
-import { ToggleButton } from './components/common';
+import { ToggleButton, Hello } from './components/common';
 
 class App extends Component {
   constructor(props) {
@@ -15,16 +20,21 @@ class App extends Component {
   render() {
     return (
       <Provider store={this.store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <ToggleButton />
-        </div>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Link to="/hello">Hello World Page</Link>
+            <Link to="/toggle">Toggle Button Page</Link>
+            <Route path="/hello" component={Hello} />
+            <Route path="/toggle" component={ToggleButton} />
+          </div>
+        </Router>
       </Provider>
     );
   }
